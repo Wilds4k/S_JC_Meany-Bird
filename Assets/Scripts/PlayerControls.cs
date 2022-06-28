64f02c38a6738d44a1b63ac4d852a3d475bb5935
Wilds4k <Strykejames950@gmail.com>
2022-06-28 18:37:33 -0500

    6-28-22

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Game Controller object for controlling the player")]
+    public GameController gameController;
+    [Header("Default Velocity")]
+    public float velocity = 1;
+    private RigidBody2D rb;
+    private float objectHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GetComponent<gameController>();
+        Time.timeScale = 1;
+        rb = GetComponent<RigidBody2D>();
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.y / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (colision.gameObject.tag == "HighSpike"
+            || collision.gameObject.tag == "LowSpike"
+            || collision == collision.gameObject.tag == "Ground")
+        {
+            GameObject.Find("GameController").GetComponent<gameController>().GameOver();
+        }
+    
+    }
+}
